d2dc1cd6af4741e81c8044a7f48dca919b15f5a1
Victor <victorspitzer@gmail.com>
2022-01-31 16:04:36 +0100

    Correction coquille source/puit

--- src/dual.jl
+++ src/dual.jl
@@ -15,18 +15,21 @@ function Dual(n :: Int64, s :: Int64, t :: Int64,  S :: Int64,  d1 :: Int64, d2 
 	@variable(m, b0 >= 0)
 
 	#Constraint on flow
-	@constraint(m,[i in 1:n ; i!=s && i!=t],sum(x[k,i]*A[k,i] - x[i,k]*A[i,k] for k in 1:n)==0)
+	@constraint(m,[i in 1:n ; i!=s && i!=t],sum(x[k,i] - x[i,k] for k in 1:n)==0)
+	for j in 1:n
+		@constraint(m,[i in 1:n],x[i,j] <= A[i,j])
+	end
 
 	#Constraint on source and well
-	@constraint(m, sum(x[s,k]*A[s,k] for k in 1:n) == 1)
-	@constraint(m, sum(x[k,t]*A[k,t] for k in 1:n) == 1)
+	@constraint(m,sum(x[s,k] - x[k,s] for k in 1:n)==1)
+	@constraint(m,sum(x[k,t] - x[t,k] for k in 1:n)==1)
 
-	@constraint(m,[ k in 1:n*n ], a0 + a[k] >= x[k])
+	@constraint(m,[ k in 1:n*n ], a0 + a[k] >= x[k]*d[k])
 
 	#dual robust constraint
-	@constraint(m, b0*d2 + 2 * sum(b[k] for k in 1:n) + sum(p[i]*sum(x[i,j]*A[i,j] for j in 1:n) for i in 1:n) <= S)
+	@constraint(m, b0*d2 + 2 * sum(b[k] for k in 1:n) + sum(p[i]*sum(x[i,j] for j in 1:n) for i in 1:n) <= S)
 
-	@constraint(m, [i in 1:n ; i!=t ], b0 + b[i] >= ph[i]*sum(x[i,k]*A[i,k] for k in 1:n))
+	@constraint(m, [i in 1:n ; i!=t ], b0 + b[i] >= ph[i]*sum(x[i,k] for k in 1:n))
 	@constraint(m, b0 + b[t] >= ph[t])
 
 	#Objectif
