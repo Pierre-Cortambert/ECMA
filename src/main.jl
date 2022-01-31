ae38b099bbf6838ee641640d1b968a9e7e36045f
Cortambert Pierre <cortambert.pierre@ensta-paris.fr>
2022-01-31 01:32:28 +0100

    arguments d'entrée

--- src/main.jl
+++ src/main.jl
@@ -1,41 +1,61 @@
 using JuMP
 using CPLEX
+using ArgParse
 
-action = Args.get(:action)
+include("read_write.jl")
+include("static.jl")
+include("dual.jl")
+include("plans_coupants.jl")
+include("branch_cut.jl")
+include("heuristic.jl")
 
-action == :dual           && @ms include("dual.jl")
-action == :plans_coupants && @ms include("plans_coupants.jl")
-action == :branch_cut     && @ms include("branch_cut.jl")
-action == :heuristic      && @ms include("heuristic.jl")
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table settings begin
+        "--algo"
+            help = "Algorithme à choisir (parmi : s, bc, d, h, pc)"
+            arg_type = String
+            default = "none"
+        "--file","-f","instance"
+            help = "Nom da l'instance à considérer"
+            arg_type = String
+            default = "20_USA-road-d.BAY.gr"
+        "--time","-t"
+            help = "Temps limite de recherche de solution optimale"
+            arg_type = Int
+            default = 0 # pas de limitation
+        "--pr" #critère d'arret correspondant au pourcentage de la solution robuste trouvé par rapport à la solution statique  : non implémentée encore
+            default = 0
+    end
+    return parse_args(ARGS, settings)
+end
 
 
 function main()
-
-
-    (n,s,t,S,d1,d2,p,ph,A,d,D)= get_data("../instances/20_USA-road-d.BAY.gr")
+    parsed_args = parse_commandline()
+    (n,s,t,S,d1,d2,p,ph,A,d,D)= get_data("../instances/"*parsed_args["file"])
     time1 = time()
 
-    action = Args.get(:action)
-    println("action = $action")
+    algo = parsed_arg["algo"]
+    if algo == "s"
+        Static(n,s,t,S,d1,d2,p,ph,A,d,D)
 
-    if action == :dual      
+    elseif algo == "d"      
         Dual(n,s,t,S,d1,d2,p,ph,A,d,D) 
     
-    elseif action == :plans_coupants 
+    elseif algo == "pc"
         PlansCoupants(n,s,t,S,d1,d2,p,ph,A,d,D)    
     
-    elseif action == :branch_cut     
-    
-    
-    else if action == :heuristic 
+    elseif algo == "bc"    
+        
+    elseif algo == :"h"
 
-    elseif action == :none
+    elseif action == "none"
         println("Aucune action indiquée")
         println(Args.get_syntaxe())
     else
         println("Erreur : action $(action) non implémentée (dans main.jl)")
-        println(Args.get_syntaxe())
-        exit(1)
     end
 
     time2 = time()
@@ -44,3 +64,4 @@ function main()
     ln1("main() END")
 end
         
+main()
