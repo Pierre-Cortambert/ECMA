b8234d22048e97f6f015da4e4e3b354df295ecce
Cortambert Pierre <cortambert.pierre@ensta-paris.fr>
2022-02-05 14:13:10 +0100

    branc and cut

--- src/branch_cut.jl
+++ src/branch_cut.jl
@@ -59,49 +59,34 @@ function BranchCut(n :: Int64, s :: Int64, t :: Int64,  S :: Int64,  d1 :: Int64
 
 	
 	#Objectif
-    @objective(m, Min, z)
-
-	#U_1=Array{Float64, 3}(zeros(2, n,n))
-	#U_2=Array{Float64, 2}(zeros(2, n))
+	@objective(m, Min, sum(x[i]*d[i] for i in 1:n*n) + z)
 
 	cpt=0
 
     function callback_bc(cb_data::CPLEX.CallbackContext, context_id::Clong)
 
-        if  context_id != CPX_CALLBACKCONTEXT_CANDIDATE
-            return 
-        end
+        if context_id == CPX_CALLBACKCONTEXT_CANDIDATE
+			CPLEX.load_callback_variable_primal(cb_data, context_id)
+            x_opt = Array{Float64,2}(zeros(n,n))
+            for i in 1:n
+                for j in 1:n
+                    x_opt[i,j] = callback_value(cb_data, x[i,j])
+                end
+            end
+			z_opt = callback_value(cb_data, z)
 
-        CPLEX.load_callback_variable_primal(cb_data, context_id)
-        x_val = callback_value.(cb_data, x)
-        z_val = callback_value.(cb_data, z)
+			dlt1, z_1 = spo(n,x_opt,d1,d,D)
+			dlt2, z_2 = spc(n,t,x_opt,d2,ph)
 
-        #cpt+=1
-		
-		#Constraint on cut set #1 
-		#@constraint(m,sum( sum(x[i,j]*d[i,j]*U_1[1,i,j] for j in 1:n) for i in 1:n)- z <= 0)
-
-		#Constraint on cut set #2 
-		#@constraint(m,sum( sum(x[i,j]*p[i]+x[i,j]*ph[i]*U_2[1,i] for j in 1:n) for i in 1:n) + p[t] + ph[t]*U_2[1,t] <= S)
-
-		optimize!(m)
-		x_opt = JuMP.value.(x)
-		z_opt = JuMP.value.(z)
-
-        dlt1, z_1 = spo(n,x_opt,d1,d,D)
-		dlt2, z_2 = spc(n,t,x_opt,d2,ph)
-
-        if abs(z_1-z_opt)<1e-4 && z_2 +  sum( sum(x_opt[i,j]*A[i,j]*p[i] for j in 1:n) for i in 1:n) + p[t] <= S #on est opt
-			return 
-		else
-            if abs(z_1-z_opt)>1e-4
+			if abs(z_1-z_opt)>1e-4
 				cstr1 = @build_constraint( sum( sum(x[i,j]*d[i,j]*(1+dlt1[i,j]) for j in 1:n) for i in 1:n)- z <= 0)
-                MOI.submit(m, MOI.LazyConstraint(cb_data), cstr1)
+				MOI.submit(m, MOI.LazyConstraint(cb_data), cstr1)
 			end
 			if z_2 +  sum( sum(x_opt[i,j]*A[i,j]*p[i] for j in 1:n) for i in 1:n) + p[t] > S 
 				cstr2 = @build_constraint( sum( sum(x[i,j]*p[i]+x[i,j]*ph[i]*dlt2[i] for j in 1:n) for i in 1:n) + p[t] + ph[t]*dlt2[t] <= S)
-                MOI.submit(m, MOI.LazyConstraint(cb_data), cstr2)
+				MOI.submit(m, MOI.LazyConstraint(cb_data), cstr2)
 			end
+			
 		end
     end
     MOI.set(m, CPLEX.CallbackFunction(), callback_bc)
