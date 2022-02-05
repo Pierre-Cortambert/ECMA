7b7a8998264de50ae5fcf366d6b6ba029654860c
Cortambert Pierre <cortambert.pierre@ensta-paris.fr>
2022-02-05 13:29:30 +0100

    bash

--- src/script.sh
+++ src/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-algorithmes = "s"
-instances = "20_USA-road-d.BAY.gr  20_USA-road-d.COL.gr  20_USA-road-d.NY.gr 
+algorithmes="s"
+instances="20_USA-road-d.BAY.gr  20_USA-road-d.COL.gr  20_USA-road-d.NY.gr 
              40_USA-road-d.BAY.gr  40_USA-road-d.COL.gr  40_USA-road-d.NY.gr 
              60_USA-road-d.BAY.gr  60_USA-road-d.COL.gr  60_USA-road-d.NY.gr
              80_USA-road-d.BAY.gr  80_USA-road-d.COL.gr  80_USA-road-d.NY.gr
